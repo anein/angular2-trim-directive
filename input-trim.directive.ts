9b06662193a9aacfc225be86ed7332246b1f1f2d
Alexander Ein <cruelty.deny@gmail.com>
2017-08-14 01:02:55 +0300

    feat: v4

--- input-trim.directive.ts
+++ input-trim.directive.ts
@@ -1,79 +1,28 @@
 import {
-  Directive, HostListener, Renderer, forwardRef, ElementRef
+  Directive, HostListener
 } from '@angular/core';
-import { NG_VALUE_ACCESSOR, DefaultValueAccessor } from '@angular/forms';
+import { DefaultValueAccessor } from '@angular/forms';
 
 @Directive( {
-  selector : '[trim]',
-  providers: [ {
-    provide    : NG_VALUE_ACCESSOR,
-    useExisting: forwardRef( () => InputTrimDirective ),
-    multi      : true
-  } ]
+  selector: '[trim]'
 } )
 export class InputTrimDirective extends DefaultValueAccessor {
 
-  private _el: HTMLInputElement;
-
   /**
-   * Creates an instance of InputTrimDirective.
-   *
-   * @param {Renderer} _renderer
-   * @param {ElementRef} _elementRef
-   *
-   * @memberOf InputTrimDirective
-   */
-  constructor ( _renderer: Renderer, _elementRef: ElementRef ) {
-
-    super( _renderer, _elementRef );
-
-    this._el = _elementRef.nativeElement;
-
-  }
-
-  /**
-   * Updates the  value on the blur event.
+   * Updates the value on the blur event.
    *
    */
-  @HostListener( 'blur', [ '$event.target.value' ] )
-  onBlur ( value: string ): void {
-    this.trimValue( value );
+  @HostListener( 'blur', ['$event.target.value'] )
+  onBlur( value: string ): void {
+    super.writeValue( value.trim() );
   }
 
   /**
    * Updates the value on the input event.
    */
-  @HostListener( 'input', [ '$event.target.value' ] )
-  updateValue ( value: string ): void {
-    this.trimValue( value );
+  @HostListener( 'input', ['$event.target.value'] )
+  onInput( value: string ): void {
+    super.writeValue( value.trim() );
   }
 
-  /**
-   * Write a value to the element.
-   *
-   * @param {string} value - new value.
-   *
-   * @memberOf InputTrimDirective
-   */
-  writeValue ( value: string ): void {
-    super.writeValue( value );
-  }
-
-  /**
-   * Trims the value and sets it to the element.
-   *
-   * @private
-   * @param {string} value - value
-   *
-   * @memberOf InputTrimDirective
-   */
-  private trimValue( value: string ): void {
-
-    value = value.trim();
-
-    this._el.value = value;
-
-    this.onChange( value );
-
-  }
 }
