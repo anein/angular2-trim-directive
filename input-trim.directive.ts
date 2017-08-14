4f6ac7cfaae10eb2a82dbd5e201f9fa7ce40cab5
Alexander Ein <cruelty.deny@gmail.com>
2017-08-14 04:44:56 +0300

    fix: set a new value to the model

--- input-trim.directive.ts
+++ input-trim.directive.ts
@@ -1,28 +1,55 @@
 import {
-  Directive, HostListener
+  Directive, EventEmitter, HostListener, Input, Output
 } from '@angular/core';
 import { DefaultValueAccessor } from '@angular/forms';
 
 @Directive( {
-  selector: '[trim]'
+  selector: 'input[trim]',
 } )
 export class InputTrimDirective extends DefaultValueAccessor {
 
+  // Get a value of the trim attribute if it was set.
+  @Input() trim: string;
+
+  // this property is necessary to update the model.
+  @Output() ngModelChange: EventEmitter<any> = new EventEmitter();
+
   /**
    * Updates the value on the blur event.
-   *
    */
-  @HostListener( 'blur', ['$event.target.value'] )
-  onBlur( value: string ): void {
-    super.writeValue( value.trim() );
+  @HostListener( 'blur', ['$event.type', '$event.target.value'] )
+  onBlur( event: string, value: string ): void {
+    this.updateValue( event, value );
   }
 
   /**
    * Updates the value on the input event.
    */
-  @HostListener( 'input', ['$event.target.value'] )
-  onInput( value: string ): void {
-    super.writeValue( value.trim() );
+  @HostListener( 'input', ['$event.type', '$event.target.value'] )
+  onInput( event: string, value: string ): void {
+    this.updateValue( event, value );
+  }
+
+  /**
+   * Trims an input value, and sets it to the model and element.
+   *
+   * @param {string} value - input value
+   * @param {string} event - input event
+   */
+  private updateValue( event: string, value: string ): void {
+
+    // check if the user has set an optional attribute.
+    if (this.trim !== '' && event !== this.trim) {
+      return;
+    }
+
+    // trim! Uhahahaha!
+    value = value.trim();
+
+    // update model
+    this.ngModelChange.emit( value );
+    // update element
+    this.writeValue( value );
   }
 
 }
