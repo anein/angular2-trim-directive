a5e8df6c4727ac7a9307fded5424ad40272ceaa0
Alexander Ein <cruelty.deny@gmail.com>
2017-08-14 04:50:55 +0300

    fix: update example

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { InputTrimDirective } from '../../../input-trim.directive';
+import { FormsModule } from '@angular/forms';
 
 @NgModule( {
-  imports     : [BrowserModule],
+  imports     : [BrowserModule, FormsModule],
   declarations: [
     AppComponent,
     InputTrimDirective
