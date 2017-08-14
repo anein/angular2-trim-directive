1b5091204e20b52ecf525059d942fe7b54827e20
Alexander Ein <cruelty.deny@gmail.com>
2017-08-14 04:50:55 +0300

    fix: update example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component( {
   'selector'   : 'in-app',
   'templateUrl': './app.component.html'
 } )
 export class AppComponent {
+
+  example: string = '';
+
+  onSubmit(): void {
+    console.log( this.example );
+    console.log( this.example.length );
+  }
 
 }
