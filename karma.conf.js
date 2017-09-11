b6a1f4f2d7ab7b40a5468b5b915ebf17749ca286
Alexander Ein <cruelty.deny@gmail.com>
2017-09-11 07:19:51 +0300

    chore(travis): set the singleRun flag

--- karma.conf.js
+++ karma.conf.js
@@ -23,8 +23,8 @@ module.exports = function (config) {
     port                    : 9876,
     colors                  : true,
     logLevel                : config.LOG_INFO,
-    autoWatch               : true,
+    autoWatch               : false,
     browsers                : ['PhantomJS'],
-    singleRun               : false
+    singleRun               : true
   });
 };
