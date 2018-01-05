41d12ec14aceb400406baefc36a83f1c7b8b87b2
Alexander Ein <435658+anein@users.noreply.github.com>
2018-01-06 00:19:08 +0300

    fix: update value of HTML5 input elements (Fix #9)

--- src/input-trim.module.ts
+++ src/input-trim.module.ts
@@ -1,6 +1,5 @@
 import { InputTrimDirective } from './input-trim.directive';
 import { NgModule } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @NgModule( {
   imports     : [],
