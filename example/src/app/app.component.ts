b00b9d3fc4582baedd3c233ae4c858650ead02aa
Alexander Ein <435658+anein@users.noreply.github.com>
2018-10-19 08:33:27 +0300

    feat: add 'autofill' and 'undefined' fields

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -1,38 +1,68 @@
-import {
-  ChangeDetectionStrategy, Component, Inject, Input,
-  ViewEncapsulation
-} from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, Inject, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component( {
   'selector'     : 'in-app',
   'templateUrl'  : './app.component.html',
-  'styles'       : ['pre { background-color: whitesmoke;}'],
+  'styles'       : ['pre { background-color: whitesmoke;} small {color: #AAA}'],
   encapsulation  : ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 } )
 export class AppComponent {
 
   trigger: FormControl;
+
   exampleForm: FormGroup;
 
+  exampleFormInfo = {};
+
   constructor( @Inject( FormBuilder ) private fb: FormBuilder ) {
-    this.trigger = this.fb.control('input');
+    this.trigger = this.fb.control( 'input' );
 
     this.exampleForm = this.fb.group( {
 
-      'text'    : ['', Validators.required],
-      'email'   : ['', [Validators.required, Validators.email]],
-      'number'  : ['', [Validators.required]],
-      'url'     : ['', [Validators.required]],
-      'textarea': ['', [Validators.required, Validators.maxLength( 10 )]]
+      'text'          : ['', Validators.required],
+      'text_undefined': [null, Validators.required],
+      'text_autofill' : [undefined, Validators.required],
+      'email'         : ['', [Validators.required, Validators.email]],
+      'number'        : ['', [Validators.required]],
+      'url'           : ['', [Validators.required]],
+      'textarea'      : ['', [Validators.required, Validators.maxLength( 10 )]]
 
     } );
+
+    this.updateStates();
+
+  }
+
+  /**
+   * ngFor Helper
+   */
+  getKeys( obj: Object ): Array<string> {
+
+    this.updateStates();
+
+    return Object.keys( obj );
+  }
+
+  /**
+   * Can be simplified
+   */
+  updateStates() {
+
+    const fields = ['status', 'dirty', 'touched'];
+
+    for (let item in this.exampleForm.controls) {
+      this.exampleFormInfo[item] = {};
+      for (let field of fields) {
+        this.exampleFormInfo[item][field] = this.exampleForm.controls[item][field];
+      }
+
+    }
 
   }
 
   onSubmit(): void {
-    //
   }
 
 }
