2c53f6c02cd0109477206146d244143ba74515d3
Alexander Ein <435658+anein@users.noreply.github.com>
2018-11-15 23:35:54 +0300

    fix: prevent setting 'undefined'. Closes #18

--- src/input-trim.directive.ts
+++ src/input-trim.directive.ts
@@ -1,20 +1,18 @@
-import {
-  Directive,
-  ElementRef,
-  HostListener,
-  Inject,
-  Input,
-  Optional,
-  Renderer2
-} from '@angular/core';
-import { COMPOSITION_BUFFER_MODE, DefaultValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Directive, ElementRef, HostListener, Inject, Input, Optional, Renderer2 } from "@angular/core";
+import { COMPOSITION_BUFFER_MODE, DefaultValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 
-@Directive( {
-  selector : 'input[trim], textarea[trim]',
+@Directive({
+  selector: "input[trim], textarea[trim]",
   providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: InputTrimDirective, multi: true }]
-
-} )
+})
 export class InputTrimDirective extends DefaultValueAccessor {
+  // Get a value of the trim attribute if it was set.
+  @Input() trim: string;
+
+  @Input()
+  set type(value: string) {
+    this._type = value || "text";
+  }
 
   /**
    * Keep the type of input element in a cache.
@@ -22,7 +20,7 @@ export class InputTrimDirective extends DefaultValueAccessor {
    * @type {string}
    * @private
    */
-  private _type: string = 'text';
+  private _type: string = "text";
 
   /**
    * Keep the value of input element in a cache.
@@ -36,74 +34,29 @@ export class InputTrimDirective extends DefaultValueAccessor {
   private _sourceRenderer: Renderer2;
   private _sourceElementRef: ElementRef;
 
-  // Get a value of the trim attribute if it was set.
-  @Input() trim: string;
-
-  @Input()
-  set type( value: string ) {
-    this._type = value || 'text';
-  }
-
-  // Get the element type
-  get type(): string {
-    return this._type;
-  }
-
-  /**
-   * Get the cached value for comparison.
-   *
-   */
-  get value() {
-    return this._value;
-  }
-
-  /**
-   * Set a new value to the field and model.
-   *
-   */
-  set value( val: any ) {
-
-    // to prevent setting `undefined` if the model is undefined or null
-    //
-    // FIX: https://github.com/anein/angular2-trim-directive/issues/18
-    //
-    val = val || '';
-
-    // update element
-    this.writeValue( val );
-
-    // update model
-    this.onChange( val );
-
-  }
-
   /**
    * Updates the value on the blur event.
    */
-  @HostListener( 'blur', ['$event.type', '$event.target.value'] )
-  onBlur( event: string, value: string ): void {
-
-    // update value if only changed
-    // FIX: https://github.com/anein/angular2-trim-directive/issues/17
-    if (value.trim() !== this.value) {
-      this.updateValue( event, value );
-    }
-
+  @HostListener("blur", ["$event.type", "$event.target.value"])
+  onBlur(event: string, value: string): void {
+    this.updateValue(event, value.trim());
     this.onTouched();
   }
 
   /**
    * Updates the value on the input event.
    */
-  @HostListener( 'input', ['$event.type', '$event.target.value'] )
-  onInput( event: string, value: string ): void {
-    this.updateValue( event, value );
+  @HostListener("input", ["$event.type", "$event.target.value"])
+  onInput(event: string, value: string): void {
+    this.updateValue(event, value);
   }
 
-  constructor( @Inject( Renderer2 ) renderer: Renderer2,
-               @Inject( ElementRef ) elementRef: ElementRef,
-               @Optional() @Inject( COMPOSITION_BUFFER_MODE ) compositionMode: boolean ) {
-    super( renderer, elementRef, compositionMode );
+  constructor(
+    @Inject(Renderer2) renderer: Renderer2,
+    @Inject(ElementRef) elementRef: ElementRef,
+    @Optional() @Inject(COMPOSITION_BUFFER_MODE) compositionMode: boolean
+  ) {
+    super(renderer, elementRef, compositionMode);
 
     this._sourceRenderer = renderer;
     this._sourceElementRef = elementRef;
@@ -114,21 +67,27 @@ export class InputTrimDirective extends DefaultValueAccessor {
    *
    * @param {any} value - new value
    */
-  public writeValue( value: any ): void {
+  public writeValue(value: any): void {
+    //
+    // The Template Driven Form doesn't automatically convert undefined values to null. We will do,
+    // keeping an empty string as string because the condition `'' || null` returns null what
+    // could change the initial state of a model.
+    // The Reactive Form does it automatically during initialization.
+    //
+    // ISSUE: https://github.com/anein/angular2-trim-directive/issues/18
+    //
+    this._value = value === "" ? "" : value || null;
 
-    this._value = value;
-
-    this._sourceRenderer.setProperty( this._sourceElementRef.nativeElement, 'value', value );
+    this._sourceRenderer.setProperty(this._sourceElementRef.nativeElement, "value", this._value);
 
     // a dirty trick (or magic) goes here:
-    // it updates the element value if `setProperty` doesn't set it for some reason.
+    // it updates the element value if `setProperty` doesn't set a new value for some reason.
     //
-    // FIX: https://github.com/anein/angular2-trim-directive/issues/9
+    // ISSUE: https://github.com/anein/angular2-trim-directive/issues/9
     //
-    if (this._type !== 'text') {
-      this._sourceRenderer.setAttribute( this._sourceElementRef.nativeElement, 'value', value );
+    if (this._type !== "text") {
+      this._sourceRenderer.setAttribute(this._sourceElementRef.nativeElement, "value", this._value);
     }
-
   }
 
   /**
@@ -137,11 +96,24 @@ export class InputTrimDirective extends DefaultValueAccessor {
    * @param {string} value - input value
    * @param {string} event - input event
    */
-  private updateValue( event: string, value: string ): void {
-
+  private updateValue(event: string, value: string): void {
     // check if the user has set an optional attribute. Trimmmm!!! Uhahahaha!
-    this.value = (this.trim !== '' && event !== this.trim) ? value : value.trim();
+    value = this.trim !== "" && event !== this.trim ? value : value.trim();
 
+    const previous = this._value;
+
+    // write value to the element.
+    this.writeValue(value);
+
+    // Update model only when getting new value, and prevent firing
+    // the `dirty` state for empty fields.
+    //
+    // ISSUE: https://github.com/anein/angular2-trim-directive/issues/17
+    //
+    // TODO: Optimize
+    //
+    if (previous && (this._value.trim() !== "" || previous.trim() !== "") && this._value.trim() !== previous) {
+      this.onChange(this._value);
+    }
   }
-
 }
