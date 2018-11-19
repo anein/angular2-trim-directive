cf76b3c44fef659974bdac228e745a594d6039b7
ShinDarth <borzifrancesco@gmail.com>
2018-11-19 11:07:04 +0100

    Replace Phantomjs with ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function (config) {
     frameworks              : ['jasmine', '@angular/cli'],
     plugins                 : [
       require('karma-jasmine'),
-      require('karma-phantomjs-launcher'),
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
@@ -24,7 +24,7 @@ module.exports = function (config) {
     colors                  : true,
     logLevel                : config.LOG_INFO,
     autoWatch               : false,
-    browsers                : ['PhantomJS'],
+    browsers                : ['ChromeHeadless'],
     singleRun               : true
   });
 };
