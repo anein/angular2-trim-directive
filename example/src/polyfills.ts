a93db9aab8bfe37013d7f143a9ae58bc42891e4f
ShinDarth <borzifrancesco@gmail.com>
2018-11-20 17:52:08 +0100

    Update to latest Angular

--- example/src/polyfills.ts
+++ example/src/polyfills.ts
@@ -39,7 +39,7 @@
 
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+
 import 'core-js/es6/symbol';
 import 'core-js/es6/object';
 import 'core-js/es6/string';
