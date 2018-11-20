17b22fba6494387d15df603b8824eb7c0bed67c2
Alexander Ein <435658+anein@users.noreply.github.com>
2018-11-20 15:00:13 +0300

    chore(travis): automate building gh-pages

---
+++ scripts/gh-pages.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# pre-deploy
+cd example/dist
+mv example/src/index.html ./index.html
+rm -rf example
+
+# config
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI Bot"
+
+#
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
